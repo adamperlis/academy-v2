4f0a5d6f7e8349641014957e658550ec52bb3fb7
Adam Perlis <adam.perlis@gmail.com>
2018-02-20 16:10:25 -0500

    fixed navigation bar

--- src/app/components/jumbotron/Jumbotron.js
+++ src/app/components/jumbotron/Jumbotron.js
@@ -1,18 +1,39 @@
-// @flow weak
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
+import Paper from 'material-ui/Paper';
+import Grid from 'material-ui/Grid';
 
-import React              from 'react';
-import PropTypes          from 'prop-types';
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing.unit * 2,
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+});
 
 const Jumbotron = (props) => {
+
+  const { classes } = props;
+
   return (
-    <div className="jumbotron">
-      {props.children}
+    <div className={classes.root}>
+      <Grid container spacing={24}>
+        <Grid item xs={12}>
+
+          {props.children}
+        </Grid>
+      </Grid>
     </div>
   );
 };
 
 Jumbotron.propTypes = {
-  children: PropTypes.node
+  classes: PropTypes.object.isRequired,
+  children: PropTypes.node,
 };
 
-export default Jumbotron;
+export default withStyles(styles)(Jumbotron);
